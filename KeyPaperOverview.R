899cc76c49e14f456f385d2d2a072b8dee57ecde
Bri <briannaljohns@gmail.com>
2026-01-20 13:34:14 +1100

    refined keywords

--- KeyPaperOverview.R
+++ KeyPaperOverview.R
@@ -1,85 +1,46 @@
 library(litsearchr)
 library(dplyr)
+library(ggplot2)
 library(ggraph)
 library(igraph)
-library(ggplot2)
+library(readr)      
 
-#loading results
-#import .ris from existing naive search on Web of Science
-keypaper_results <- import_results(file="LitSearchRWoSNaiveResults.ris")
 
-# #double check that .ris file loaded correctly
-# nrow(naive_results)
-# naive_results
-# colnames(naive_results)
-# naive_results[1, "title"]
+#import .ris from existing search on Web of Science;
+results <- import_results(file="keypapers.ris")
 
-#title terms
-# get common stopwords from litsearchr
-all_stopwords <- get_stopwords("English")
-extract_terms(text=naive_results[, "title"], method="fakerake", min_freq=3, min_n=2, stopwords = all_stopwords) 
+# print(results)
 
+# double check that .ris file loaded correctly
+# nrow(results)
+# colnames(results)
+# results[1, "keywords"]
+
+# Extract keywords
+keywords <- extract_terms(keywords = results[, "keywords"], method = "tagged", min_n = 1)
+
+keywords
+
+# Extract key terms from titles
 title_terms <- extract_terms(
-  text=naive_results[, "title"],
+  text=results[, "title"],
   method="fakerake",
-  min_freq=3, min_n=2,
-  stopwords=all_stopwords
-)
+  min_freq=2, min_n=1,)
 
 title_terms
 
-terms <- unique(title_terms)
+# Extract key terms from abstract
 
-# network analysis
-docs <- paste(naive_results[, "title"], naive_results[, "abstract"])
-docs[1]
+all_stopwords <- get_stopwords("English")
 
-dfm <- create_dfm(elements=docs, features=terms)
-dfm[1:3, 1:4]
+abstract <- extract_terms(
+  text=results[, "abstract"],
+  method="fakerake",
+  min_freq=3, min_n=1,
+  stopwords=all_stopwords)
 
-# create network graph
+abstract
 
-g <- create_network(dfm, min_studies=3)
+all_terms <- unique(c(keywords, title_terms, abstract))
 
-ggraph(g, layout="stress") +
-  coord_fixed() +
-  expand_limits(x=c(-3, 3)) +
-  geom_edge_link(aes(alpha=weight)) +
-  geom_node_point(shape="circle filled", fill="white") +
-  geom_node_text(aes(label=name), hjust="outward", check_overlap=TRUE) +
-  guides(edge_alpha=FALSE)
-
-#rank search terms
-strengths <- strength(g)
-
-data.frame(term=names(strengths), strength=strengths, row.names=NULL) %>%
-  mutate(rank=rank(strength, ties.method="min")) %>%
-  arrange(strength) ->
-  term_strengths
-
-term_strengths
-cutoff_fig <- ggplot(term_strengths, aes(x=rank, y=strength, label=term)) +
-  geom_line() +
-  geom_point() +
-  geom_text(data=filter(term_strengths, rank>5), hjust="right", nudge_y=20, check_overlap=TRUE)
-
-cutoff_fig
-
-cutoff_cum <- find_cutoff(g, method="cumulative", percent=0.8)
-
-cutoff_cum
-
-cutoff_fig +
-  geom_hline(yintercept=cutoff_cum, linetype="dashed")
-
-get_keywords(reduce_graph(g, cutoff_cum))
-
-cutoff_change <- find_cutoff(g, method="changepoint", knot_num=3)
-cutoff_change
-cutoff_fig +
-  geom_hline(yintercept=cutoff_change, linetype="dashed")
-
-g_redux <- reduce_graph(g, cutoff_change[1])
-selected_terms <- get_keywords(g_redux)
-
-selected_terms
+all_terms
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 R code for using litsearchr to maximise search strategy for global synthesis of open tech in insect monitoring
+
+[Tutorial for using litsearchr](https://luketudge.github.io/litsearchr-tutorial/litsearchr_tutorial.html)
